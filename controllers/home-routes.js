52b9b130a1128511210ec1bcf7b4a2a0acc9f88d
Connor Mullan <cmullan602@gmail.com>
2022-09-14 20:54:00 -0700

    added homepage routes.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Image, Comment } = require('../models');
 const {withAuth, withAuthApi } = require('../utils/auth');
 
 
 
-// TODO: Add a comment describing the functionality of the withAuth middleware
 router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
@@ -24,7 +23,6 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // TODO: Add a comment describing the functionality of this if statement
   if (req.session.logged_in) {
     res.redirect('/');
     return;
@@ -33,7 +31,62 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+//gallery route
+router.get('/gallery', withAuth, async (req, res) => {
+  try {
+    const dbImageData = await Image.findAll({
+     where: {
+         user_id: req.session.user_id
+     }
+    });
 
+    const images = dbImageData.map((image) =>
+      image.get({ plain: true })
+    );
+
+    res.render('gallery', {
+      layout: 'main',
+      images,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+//Image post Route
+router.get('/post/:id', async (req, res) => {
+  Image.findByPk(req.params.id, {
+       include: [
+       User,
+         {
+           model: Comment,
+           include: {
+             model: User
+           }
+         },
+         
+       ],
+     })
+     .then((dbImageData) => {
+       if (dbImageData) {
+         const image = dbImageData.get({ plain: true });
+
+         console.log(image)
+ 
+         res.render("image-post", { 
+           post,
+           logged_in: req.session.logged_in });
+       } else {
+         res.status(404).end();
+       }
+     })
+     .catch((err) => {
+       res.status(500).json(err);
+     });
+ });
 
 module.exports = router;
 
