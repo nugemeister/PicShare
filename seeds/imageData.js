d624d4b633a79b2a489eff02c130972ef574ab8b
Connor Mullan <cmullan602@gmail.com>
2022-09-14 21:30:49 -0700

    finalized gallery handlebar.

--- seeds/imageData.js
+++ seeds/imageData.js
@@ -2,50 +2,50 @@ const { Image } = require('../models');
 
 const imageData = [
   {
-    title: '../public/images/city-pic.jpg',
+    file_name: '/images/city-pic.jpg',
     user_id: 1,
-    file_name: 'city-pic.jpg',
+    title: '/images/city-pic.jpg',
     
   },
   {
-    title: '../public/images/forest-pic.jpg',
+    file_name: '/images/forest-pic.jpg',
     user_id: 1,
-    file_name: 'forest-pic.jpg',
+    title: '/images/forest-pic.jpg',
   },
   {
+    file_name: '/images/mountain-dog.jpg',
+    user_id: 2,
     title: '/images/mountain-dog.jpg',
-    user_id: 2,
-    file_name: 'mountain-dog.jpg',
   },
   {
-    title: '/images/nice-pic.jpg',
+    file_name: '/images/nice-pic.jpg',
     user_id: 2,
-    file_name: 'nice-pic.jpg',
+    title: 'nice-pic.jpg',
   },
   {
-    title: '/images/sunflower-pic.jpg',
+    file_name: '/images/sunflower-pic.jpg',
     user_id: 2,
-    file_name: 'sunflower-pic.jpg',
+    title: 'sunflower-pic.jpg',
   },
   {
-    title: '/images/water-pic.jpg',
+    file_name: '/images/water-pic.jpg',
     user_id: 3,
-    file_name: 'water-pic.jpg',
+    title: 'water-pic.jpg',
   },
   {
-    title: '/images/sunset-pic.jpg',
+    file_name: '/images/sunset-pic.jpg',
     user_id: 3,
-    file_name: 'sunset-pic.jpg',
+    title: 'sunset-pic.jpg',
   },
   {
-    title: '/images/tree-pic.jpg',
+    file_name: '/images/tree-pic.jpg',
     user_id: 4,
-    file_name: 'tree-pic.jpg',
+    title: 'tree-pic.jpg',
   },
   {
-    title: '/images/wut-pic.jpg',
+    file_name: '/images/wut-pic.jpg',
     user_id: 5,
-    file_name: 'wut-pic.jpg',
+    title: 'wut-pic.jpg',
   },
 ];
 
