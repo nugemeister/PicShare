51f4cc7001b386a89fd0ee3f620b1a5f16a16e97
Connor Mullan <cmullan602@gmail.com>
2022-09-17 10:35:55 -0700

    finalized link changes and routes.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,27 +4,15 @@ const {withAuth, withAuthApi } = require('../utils/auth');
 
 
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] }
-    });
+router.get('/', (req, res) => {
 
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      // TODO: Add a comment describing the functionality of this property
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  res.render('homepage');
 });
+
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/gallery');
     return;
   }
 
